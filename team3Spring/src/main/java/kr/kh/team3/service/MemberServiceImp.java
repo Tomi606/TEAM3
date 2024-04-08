5f2d597798b69022cf8f9a0574100a3e54f91298
KyungHO123 <152416402+KyungHO123@users.noreply.github.com>
2024-04-08 17:30:32 +0900

    비번 암호화 완료

--- team3Spring/src/main/java/kr/kh/team3/service/MemberServiceImp.java
+++ team3Spring/src/main/java/kr/kh/team3/service/MemberServiceImp.java
@@ -3,6 +3,7 @@ package kr.kh.team3.service;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import kr.kh.team3.dao.MemberDAO;
@@ -14,42 +15,52 @@ import kr.kh.team3.model.vo.SiteManagement;
 
 @Service
 public class MemberServiceImp implements MemberService {
-	
-	
+
+	@Autowired
+	BCryptPasswordEncoder passwordEncoder;
 
 	@Autowired
 	MemberDAO memberDao;
 
 	public boolean memberSignup(MemberVO member) {
-		if(member == null || member.getMe_id().length()==0)
+		if (member == null)
 			return false;
-		
-		return memberDao.insertMember(member);
+		MemberVO user = memberDao.selectMember(member.getMe_id());
+		if (user != null)
+			return false;
+		if (member.getMe_pw() == null || member.getMe_pw().length() == 0)
+			return false;
+		String encPw = passwordEncoder.encode(member.getMe_pw());
+		member.setMe_pw(encPw);
+		try {
+			return memberDao.insertMember(member);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
-
 	public boolean siteSignup(SiteManagement site) {
 		return memberDao.insertSiteMember(site);
 	}
 
 	@Override
 	public SiteManagement login(MemberVO member) {
-		//매개변수 null 처리
-		if( member == null || 
-			member.getMe_id() == null || 
-			member.getMe_pw() == null)
+		// 매개변수 null 처리
+		if (member == null || member.getMe_id() == null || member.getMe_pw() == null)
 			return null;
-		//아이디 확인
+		// 아이디 확인
 		MemberVO user = memberDao.selectMember(member.getMe_id());
-		if(user == null)
+		if (user == null)
 			return null;
-		//비번 확인
-		//맞으면 site 정보 return
-		if(member.getMe_pw().equals(user.getMe_pw())) {
-			
+		// 비번 확인
+		// 맞으면 site 정보 return
+		if (member.getMe_pw().equals(user.getMe_pw())) {
+
 			return memberDao.selectSite(user.getMe_id());
 		}
 		return null;
 	}
+
 	@Override
 	public ArrayList<SiDoVO> getSiDo() {
 		return memberDao.selectSiDo();
@@ -57,11 +68,11 @@ public class MemberServiceImp implements MemberService {
 
 	@Override
 	public ArrayList<SiGoonGuVO> getSgg() {
-		return  memberDao.selectSgg();
+		return memberDao.selectSgg();
 	}
 
 	@Override
 	public ArrayList<EupMyeonDongVO> getEmd() {
-		return  memberDao.selectEmd();
+		return memberDao.selectEmd();
 	}
 }
