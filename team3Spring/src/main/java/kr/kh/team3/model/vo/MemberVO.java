fd45f642e5e3d6309e0cc4129844cbb3967a06da
KyungHO123 <152416402+KyungHO123@users.noreply.github.com>
2024-04-14 05:06:15 +0900

    home수정

--- team3Spring/src/main/java/kr/kh/team3/model/vo/MemberVO.java
+++ team3Spring/src/main/java/kr/kh/team3/model/vo/MemberVO.java
@@ -2,13 +2,12 @@ package kr.kh.team3.model.vo;
 
 import java.util.Date;
 
-import lombok.AllArgsConstructor;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class MemberVO {
 	private String me_id;
 	private int me_hs_num; //병원 과목 번호
